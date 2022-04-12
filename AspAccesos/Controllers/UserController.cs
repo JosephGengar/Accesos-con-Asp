02bcc19e84a2e8b6ede58c242e4fdd10f8875fce
Joseph <josewsq101@gmail.com>
2022-04-12 00:25:38 -0300

    login terminado

--- AspAccesos/Controllers/UserController.cs
+++ AspAccesos/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AspAccesos.Models;
 
 namespace AspAccesos.Controllers
 {
@@ -12,6 +13,32 @@ namespace AspAccesos.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string usuario, string password)
+        {
+            try
+            {
+                using (AccesosContext db = new AccesosContext())
+                {
+                    var lst = (from d in db.TUsuarios
+                               where d.Usuario == usuario & d.Password == password & d.IdEstado == 1
+                               select d);
+                    if (lst.Count() > 0)
+                    {
+                        Session["user"] = lst.FirstOrDefault();
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario y/o Contraseña incorrectas");
+                    }
+                }           
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
         }
     }
 }
